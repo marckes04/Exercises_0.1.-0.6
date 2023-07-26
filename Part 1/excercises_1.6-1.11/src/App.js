fa9965ee494e9a6ff5c7f506ab86ea702e9d8c32
marckes04 <maresp95@gmail.com>
2023-07-26 00:02:02 -0500

    first commit

--- Part 1/excercises_1.6-1.11/src/App.js
+++ Part 1/excercises_1.6-1.11/src/App.js
@@ -31,17 +31,13 @@ const App = () => {
   }
 
 
-  
-
   const DisplayGood = props => <div> <p>Goods: {props.good} </p></div>
   const DisplayNeutral = props => <div> <p>Neutral:{props.neutral} </p></div>
   const DisplayBad = props => <div><p>Bads: {props.bad}</p></div>
   const Total = good + neutral+bad;
   const Average = ((good * 1)+ (neutral * 0) + (bad *-1)) / Total; 
   const Percentage = (good * 100)/ Total;
-
-
-  return (
+    return (
     <div>  
         <h1>Give Feedback</h1>
         <Button handleClick = {() => setToGood(good + 1)} text= "good" /> 
@@ -54,8 +50,10 @@ const App = () => {
         <p>Total: {Total}</p>
         <p>Average: {Average}</p>
         <p>Percentage: {Percentage} % </p>
-    </div>
-  )
-}
+
+      </div>
+    )}
+
+        
 
 export default App
